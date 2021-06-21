e680139fda1b3d38c056fc681c882c2909a11761
dhtnthu9794048@gmail.com <MayoG>
2021-06-21 13:42:50 +0300

    Remove built

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index","modules"],envversion:53,filenames:["index.rst","modules.rst"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,pipert2:0,search:0},titles:["Welcome to PipeRT\u2019s documentation!","pipert2"],titleterms:{document:0,indic:0,pipert2:1,pipert:0,tabl:0,welcom:0}})
