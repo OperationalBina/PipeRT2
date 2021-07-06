61c629d09b9dd3a675452e6c727b284e4d1ca589
dhtnthu9794048@gmail.com <MayoG>
2021-07-06 10:48:31 +0300

    Add new dummy object

---
+++ src/pipert2/utils/dummy_object.py
@@ -0,0 +1,7 @@
+class Dummy(object):
+    """An object that have any attribute
+    Useful for being a default value in unset parameter.
+
+    """
+    def do(self, *args, **kw): return self
+    def __getattr__(self, _): return self.do
