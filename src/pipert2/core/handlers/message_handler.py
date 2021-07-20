13d469ed4cb6635c768fee259c13ba5a14c5ddaa
Elon-Abulafia <elon945@gmail.com>
2021-07-20 15:43:15 +0300

    Finilize interface implementation

--- src/pipert2/core/handlers/message_handler.py
+++ src/pipert2/core/handlers/message_handler.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from pipert2.core.base.message import Message
 
 
 class MessageHandler(ABC):
@@ -8,13 +9,27 @@ class MessageHandler(ABC):
     and not with other routines directly.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, input_obj, output_obj):
+        self.input = input_obj
+        self.output = output_obj
 
     @abstractmethod
     def get(self):
+        """Returns the message from the input object.
+
+        Returns:
+            A message object.
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def put(self):
+    def put(self, message: Message):
+        """Puts a given message into the output object.
+
+        Args:
+            message: The message to be sent.
+
+        """
+
         raise NotImplementedError
