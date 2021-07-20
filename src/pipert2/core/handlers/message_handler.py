b744fc0f5f7ac18cbbb5f2008e38b3adbd5d9521
Elon-Abulafia <elon945@gmail.com>
2021-07-20 14:03:01 +0300

    Create initial message handler interface implementation

--- src/pipert2/core/handlers/message_handler.py
+++ src/pipert2/core/handlers/message_handler.py
@@ -2,4 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class MessageHandler(ABC):
-    pass
+    """The message handler is responsible for receiving messages
+    and relaying them onward if needed.
+    The message handler communicates only with the network object
+    and not with other routines directly.
+    """
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def put(self):
+        raise NotImplementedError
