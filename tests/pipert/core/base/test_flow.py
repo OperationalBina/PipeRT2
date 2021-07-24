27aae49c62ee12466b079b5336e5433ed241ff7d
dhtnthu9794048@gmail.com <MayoG>
2021-07-24 23:30:16 +0300

    Create execute event test

--- tests/pipert/core/base/test_flow.py
+++ tests/pipert/core/base/test_flow.py
@@ -34,23 +34,27 @@ def dummy_flow_with_two_routines(mocker: MockerFixture):
 
 
 def test_run(mocker, dummy_flow_with_two_routines: Flow):
-    start_event_mock = mocker.MagicMock()
-    dummy_flow_with_two_routines.get_events()[START_EVENT] = {start_event_mock}
+    start_event_callback_mock = mocker.MagicMock()
+    dummy_flow_with_two_routines.get_events()[START_EVENT] = {start_event_callback_mock}
 
-    stop_event_mock = mocker.MagicMock()
-    dummy_flow_with_two_routines.get_events()[STOP_EVENT] = {stop_event_mock}
+    stop_event_callback_mock = mocker.MagicMock()
+    dummy_flow_with_two_routines.get_events()[STOP_EVENT] = {stop_event_callback_mock}
 
     routine = dummy_flow_with_two_routines.routines[ROUTINE_NAME]
-    mocker.spy(routine, "execute_event")
-
-    flow_event_handler = dummy_flow_with_two_routines.event_handler
-    flow_logger = dummy_flow_with_two_routines.logger
 
     dummy_flow_with_two_routines.run()
 
     assert routine.execute_event.call_count == 4
     assert routine.execute_event.call_args_list == [call(START_EVENT), call(EVENT1), call(KILL_EVENT), call("stop")]
 
-    start_event_mock.assert_called_once()
-    stop_event_mock.assert_called_once()
+    start_event_callback_mock.assert_called_once()
+    stop_event_callback_mock.assert_called_once()
 
+
+def test_execute_event(dummy_flow_with_two_routines: Flow):
+    EVENT_NAME = "MAYO"
+
+    dummy_flow_with_two_routines.execute_event(EVENT_NAME)
+    routine_mocker = dummy_flow_with_two_routines.routines[ROUTINE_NAME]
+
+    routine_mocker.execute_event.assert_called_with(EVENT_NAME)
