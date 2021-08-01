078c87e78329dcebee969e322d43f368969afbea
dhtnthu9794048@gmail.com <MayoG>
2021-08-01 10:01:08 +0300

    Add implementation of data_class for older versions of python

--- src/pipert2/utils/method_data.py
+++ src/pipert2/utils/method_data.py
@@ -1,4 +1,9 @@
-from dataclasses import dataclass, field
+import sys
+
+if sys.version_info.minor >= 7:
+    from dataclasses import dataclass, field
+else:
+    from src.pipert2.utils.data_class.dataclasses import dataclass, field
 
 
 @dataclass
