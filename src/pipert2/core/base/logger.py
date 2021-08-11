df83ee556d853c19c80448ef60f1924ba086b02c
dhtnthu9794048@gmail.com <MayoG>
2021-08-11 11:11:09 +0300

    Fix CR notes, Add docs and type hints

--- src/pipert2/core/base/logger.py
+++ src/pipert2/core/base/logger.py
@@ -2,27 +2,67 @@ from abc import ABC, abstractmethod
 
 
 class PipeLogger(ABC):
+    """An interface for loggers implementations in the pipe.
+    Every logger that the user want to use in the pipe must follow this interface methods.
+
+    """
 
     @abstractmethod
-    def info(self, message):
+    def info(self, message: str):
+        """Log a message with severity of 'INFO'.
+
+        :param message: Message to log.
+
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def error(self, message):
+    def error(self, message: str):
+        """Log a message with severity of 'ERROR'.
+
+        :param message: Message to log.
+
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def debug(self, message):
+    def debug(self, message: str):
+        """Log a message with severity of 'DEBUG'.
+
+        :param message: Message to log.
+
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def warning(self, message):
+    def warning(self, message: str):
+        """Log a message with severity of 'WARNING'.
+
+        :param message: Message to log.
+
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def exception(self, message):
+    def exception(self, message: str):
+        """Log a message with severity of 'ERROR' with exception information.
+
+        :param message: Message to log.
+
+        """
+
         raise NotImplementedError
 
     @abstractmethod
-    def get_logger_child(self, child_name):
+    def get_logger_child(self, child_name: str):
+        """Get a logger which is a descendant to this one.
+
+        :param child_name: The name of the descendant logger.
+
+        """
+
         raise NotImplementedError
