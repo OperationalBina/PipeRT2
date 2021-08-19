fe667c707ad08f3a0d398f46cb2093ea30a12269
MayoG <dhtnthu9794048@gmail.com>
2021-08-19 14:50:14 +0300

    Change payload logic

--- src/pipert2/core/base/payload.py
+++ src/pipert2/core/base/payload.py
@@ -1,6 +1,3 @@
-from src.pipert2.core.managers.shared_memory import SharedMemory
-
-
 class Payload:
     """The payload object is what actually stores the data.
     It is responsible for encoding and decoding the data itself.
@@ -21,7 +18,6 @@ class Payload:
         """
 
         self.encoded = False
-        self.date_saved_in_shared_memory_metadata = {}
         self.data = data
 
     @property
@@ -30,34 +26,27 @@ class Payload:
 
     @data.setter
     def data(self, new_data) -> None:
+        if self.encoded:
+            self.encoded = False
+
         self._data = new_data
-        self.date_saved_in_shared_memory_metadata = {}
 
-        for data_name, data_value in new_data.items():
-            if SharedMemory.does_data_saved_in_shared_memory(data_value):
-                self.date_saved_in_shared_memory_metadata[data_name] = SharedMemory.get_data_metadata(data_value)
-
-    def decode(self) -> None:
+    def decode(self, decoder) -> None:
         """Decode the payload's data
 
         """
 
         if self.encoded:
-            for data_in_shared_memory_name in self.date_saved_in_shared_memory_metadata.keys():
-                self._data["data_in_shared_memory_name"] = \
-                    SharedMemory.get_data(data_address=self._data["data_in_shared_memory_name"],
-                                          metadata=self.date_saved_in_shared_memory_metadata[data_in_shared_memory_name])
+            self._data = decoder.decode(self._data)
 
             self.encoded = False
 
-    def encode(self) -> None:
+    def encode(self, encoder) -> None:
         """Encode the payload's data
 
         """
 
         if not self.encoded:
-            for data_in_shared_memory_name in self.date_saved_in_shared_memory_metadata.keys():
-                data_address = SharedMemory.save_data(self._data[data_in_shared_memory_name])
-                self._data[data_in_shared_memory_name] = data_address
+            self._data = encoder.encode(self._data)
 
             self.encoded = True
