4bcd4c4f4fdf7e3acd1bfbfa8f39bf04530f73dd
Elon-Abulafia <elon945@gmail.com>
2021-08-25 11:25:17 +0300

    Add tests for data transmitters and shared mem

---
+++ tests/pipert/core/base/test_data_transmitter.py
@@ -0,0 +1,40 @@
+import sys
+import pytest
+from src.pipert2.core.base.data_transmitter import DataTransmitter  # TODO: remove
+from src.pipert2.core.base.basic_transmitter import BasicTransmitter
+
+
+@pytest.fixture
+def dummy_shared_memory_transmitter():
+    if sys.version_info.minor <= 7:
+        from src.pipert2.core.base.posix_ipc_transmitter import PosixIpcTransmitter
+        dummy_shared_memory_transmitter = PosixIpcTransmitter()
+    else:
+        dummy_shared_memory_transmitter = DataTransmitter()  # TODO: Change to multiprocessing
+
+    return dummy_shared_memory_transmitter
+
+
+@pytest.fixture
+def dummy_basic_data_transmitter():
+    dummy_basic_data_transmitter = BasicTransmitter()
+    return dummy_basic_data_transmitter
+
+
+def test_shared_memory_transmit_receive(dummy_shared_memory_transmitter):
+    transmit_func = dummy_shared_memory_transmitter.transmit()
+    receive_func = dummy_shared_memory_transmitter.receive()
+
+    if sys.version_info.minor <= 7:  # TODO: Add multiprocessing test
+        data = {"data": b"AAA" * 5000, "short_data": b"AAA"}
+        return_data = transmit_func(data)
+        assert data == receive_func(return_data)
+
+
+def test_basic_transmit_receive(dummy_basic_data_transmitter):
+    transmit_func = dummy_basic_data_transmitter.transmit()
+    receive_func = dummy_basic_data_transmitter.receive()
+
+    data = {"data": b"AAA" * 5000, "short_data": b"AAA"}
+    return_data = transmit_func(data)
+    assert data == receive_func(return_data)
---
+++ tests/pipert/core/utils/test_shared_memory_manager.py
@@ -0,0 +1,37 @@
+import pytest
+from src.pipert2.utils.shared_memory_manager import SharedMemoryManager
+
+
+MAX_SEGMENT_COUNT = 5
+
+
+@pytest.fixture
+def dummy_shared_memory_manager():
+    dummy_shared_memory_manager = SharedMemoryManager(max_segment_count=MAX_SEGMENT_COUNT, segment_size=500)
+    return dummy_shared_memory_manager
+
+
+def test_cleanup(dummy_shared_memory_manager):
+    dummy_shared_memory_manager.cleanup_memory()
+    assert dummy_shared_memory_manager.shared_memory_generator.shared_memories == {}
+
+
+def test_max_count(dummy_shared_memory_manager):
+    test_data = b"AAA"
+    test_data_2 = b"aaa"
+    first_memory = dummy_shared_memory_manager.write_to_mem(test_data)
+
+    for _ in range(MAX_SEGMENT_COUNT - 1):
+        dummy_shared_memory_manager.write_to_mem(test_data_2)
+
+    assert test_data == dummy_shared_memory_manager.read_from_mem(first_memory, len(test_data))
+    dummy_shared_memory_manager.write_to_mem(test_data_2)
+    assert test_data != dummy_shared_memory_manager.read_from_mem(first_memory, len(test_data))
+    dummy_shared_memory_manager.cleanup_memory()
+
+
+def test_write_and_read_from_memory(dummy_shared_memory_manager):
+    test_data = b"AAA"
+    memory_name = dummy_shared_memory_manager.write_to_mem(test_data)
+    assert dummy_shared_memory_manager.read_from_mem(memory_name, len(test_data)) == test_data
+    dummy_shared_memory_manager.cleanup_memory()
