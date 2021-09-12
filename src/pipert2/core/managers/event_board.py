18005cfc1c00d156d5a4e8389320ade4e8935875
Bina3 <tomerdayan168@gmail.com>
2021-09-12 15:25:30 +0300

    add test to event board

--- src/pipert2/core/managers/event_board.py
+++ src/pipert2/core/managers/event_board.py
@@ -69,24 +69,12 @@ class EventBoard:
 
         """
 
-        def notify_event(event_name, output_event_queue, flow_name=None, routine_names=None, **params):
-
-            if flow_name:
-                specific_flows_to_routines = {flow_name: routine_names}
-            else:
-                specific_flows_to_routines = None
-
-            output_event_queue.put(Method(event_name, routines_by_flow=specific_flows_to_routines, params=params))
+        def notify_event(event_name, output_event_queue, routines_by_flow: dict = None, **params):
+            output_event_queue.put(Method(event_name, routines_by_flow=routines_by_flow, params=params))
 
         return partial(notify_event, output_event_queue=self.new_events_queue)
 
-    def notify_event(self, event_name, flow_name=None, routine_names=None, **params):
-
-        if flow_name:
-            specific_flows_to_routines = {flow_name: routine_names}
-        else:
-            specific_flows_to_routines = None
-
+    def notify_event(self, event_name, routines_by_flow: dict = None, **params):
         self.new_events_queue.put(Method(event_name=event_name,
-                                         routines_by_flow=specific_flows_to_routines,
+                                         routines_by_flow=routines_by_flow,
                                          params=params))
--- tests/pipert/core/utils/events_utils.py
+++ tests/pipert/core/utils/events_utils.py
@@ -1,3 +1,6 @@
+import pytest
+from pytest_mock import MockerFixture
+
 from src.pipert2.utils.consts.event_names import START_EVENT_NAME, KILL_EVENT_NAME, STOP_EVENT_NAME
 from src.pipert2.utils.method_data import Method
 
@@ -7,3 +10,5 @@ EVENT1 = Method(event_name=EVENT1_NAME)
 START_EVENT = Method(event_name=START_EVENT_NAME)
 STOP_EVENT = Method(event_name=STOP_EVENT_NAME)
 KILL_EVENT = Method(event_name=KILL_EVENT_NAME)
+
+
