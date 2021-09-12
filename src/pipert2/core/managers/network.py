a4a3d813850bdd7509e43c3bbb451041b1770f90
Elon-Abulafia <elon945@gmail.com>
2021-09-12 11:50:29 +0300

    Fix imports

--- src/pipert2/core/managers/network.py
+++ src/pipert2/core/managers/network.py
@@ -1,8 +1,8 @@
 from typing import Tuple
 from abc import ABC, abstractmethod
 from collections import defaultdict
-from pipert2.core.base.routine import Routine
-from pipert2.core.handlers.message_handler import MessageHandler
+from src.pipert2.core.base.routine import Routine
+from src.pipert2.core.handlers.message_handler import MessageHandler
 
 
 class Network(ABC):
