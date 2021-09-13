241b2724397a5b08bec446e7c283e99b313ab95b
Elon-Abulafia <elon945@gmail.com>
2021-09-13 16:02:43 +0300

    Remove unused import

--- src/pipert2/core/managers/networks/queue_network.py
+++ src/pipert2/core/managers/networks/queue_network.py
@@ -1,5 +1,4 @@
 from typing import Tuple
-from multiprocessing import Queue
 from src.pipert2.core.base.routine import Routine
 from src.pipert2.core.base.data_transmitter import DataTransmitter
 from src.pipert2.core.managers.network import Network
