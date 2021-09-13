6e58f242914d6f6887319041e7520e553b667e0b
MayoG <dhtnthu9794048@gmail.com>
2021-09-13 10:02:20 +0300

    Removed dump tests

--- tests/pipert/core/base/test_payload.py
+++ tests/pipert/core/base/test_payload.py
@@ -14,30 +14,3 @@ def dummy_payload():
     return payload
 
 
-@pytest.fixture()
-def encoded_payload(dummy_payload: Payload):
-    encoder_mock = Mock()
-    encoder_mock.encode.side_effect = lambda val: val
-    dummy_payload.encode(encoder_mock)
-    return dummy_payload
-
-
-def test_encode_payload(dummy_payload: Payload):
-    assert not dummy_payload.encoded
-    encoder_mock = Mock()
-    dummy_payload.encode(encoder_mock)
-
-    encoder_mock.encode.assert_called_with(DATA)
-    assert dummy_payload.encoded
-
-
-def test_decode_payload(encoded_payload: Payload):
-    assert encoded_payload.encoded
-    decoder_mock = Mock()
-    encoded_payload.decode(decoder_mock)
-
-    decoder_mock.decode.assert_called_with(DATA)
-    assert not encoded_payload.encoded
-
-
-
