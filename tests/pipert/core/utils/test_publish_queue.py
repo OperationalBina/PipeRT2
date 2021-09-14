a64eb3f35324f26d3e394a0ad1a979e6b097db16
Elon-Abulafia <elon945@gmail.com>
2021-09-14 14:16:34 +0300

    Add tests

--- tests/pipert/core/utils/test_publish_queue.py
+++ tests/pipert/core/utils/test_publish_queue.py
@@ -1,21 +1,45 @@
+from queue import Full
+
+import pytest
 from src.pipert2.utils.publish_queue import PublishQueue
 
 
-def test_single_consumer():
-    publish_queue = PublishQueue()
+@pytest.fixture()
+def dummy_publish_queue():
+    dummy_publish_queue = PublishQueue()
 
-    single_queue = publish_queue.register(None)
-    publish_queue.put("message")
+    return dummy_publish_queue
+
+
+def test_single_consumer(dummy_publish_queue):
+    single_queue = dummy_publish_queue.register(None)
+    dummy_publish_queue.put("message")
 
     assert single_queue.get() == "message"
 
 
-def test_multiple_consumer():
-    publish_queue = PublishQueue()
-
-    queue1 = publish_queue.register(None)
-    queue2 = publish_queue.register(None)
-    queue3 = publish_queue.register(None)
-    publish_queue.put("message")
+def test_multiple_consumer(dummy_publish_queue):
+    queue1 = dummy_publish_queue.register(None)
+    queue2 = dummy_publish_queue.register(None)
+    queue3 = dummy_publish_queue.register(None)
+    dummy_publish_queue.put("message")
 
     assert queue1.get() == queue2.get() == queue3.get() == "message"
+
+
+def test_force_push(dummy_publish_queue):
+    queue = dummy_publish_queue.register(None)
+    dummy_publish_queue.put("1")
+    dummy_publish_queue.put("2")
+
+    assert queue.get() == "2"
+
+
+def test_blocking_put(dummy_publish_queue):
+    queue = dummy_publish_queue.register(None)
+    dummy_publish_queue.put("1", block=True)
+
+    with pytest.raises(Full):
+        dummy_publish_queue.put("2", block=True)
+
+    assert queue.get() == "1"
