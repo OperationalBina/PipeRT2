39de05ab5fec2c54e5a50c63a42db3087f516ad5
MayoG <dhtnthu9794048@gmail.com>
2021-09-29 11:47:40 +0300

    Add new test for build and join of the event board

--- tests/pipert/core/managers/test_event_board.py
+++ tests/pipert/core/managers/test_event_board.py
@@ -1,5 +1,7 @@
 import pytest
+from mock import patch
 from src.pipert2.core.managers.event_board import EventBoard
+from src.pipert2.utils.dummy_object import Dummy
 from tests.pipert.core.utils.events_utils import EVENT1, START_EVENT, KILL_EVENT, STOP_EVENT
 
 EVENTS = [START_EVENT, EVENT1, STOP_EVENT, KILL_EVENT]
@@ -11,17 +13,30 @@ def dummy_event_board():
     return dummy_event_board
 
 
-def test_event_notifier(dummy_event_board: EventBoard):
-    event_notifier = dummy_event_board.get_event_notifier()
+@pytest.fixture()
+def dummy_event_board_no_thread():
+    with patch('src.pipert2.core.managers.event_board.Thread', return_value=Dummy()):
+        dummy_event_board = EventBoard()
+        yield dummy_event_board
+
+
+def test_event_notifier(dummy_event_board_no_thread: EventBoard):
+    event_notifier = dummy_event_board_no_thread.get_event_notifier()
     event_notifier(EVENT1.event_name)
 
-    assert dummy_event_board.new_events_queue.get() == EVENT1
+    assert dummy_event_board_no_thread.new_events_queue.get() == EVENT1
 
 
-def test_notify_event(dummy_event_board: EventBoard):
-    dummy_event_board.notify_event(EVENT1.event_name)
+def test_notify_event(dummy_event_board_no_thread: EventBoard):
+    dummy_event_board_no_thread.notify_event(EVENT1.event_name)
 
-    assert dummy_event_board.new_events_queue.get() == EVENT1
+    assert dummy_event_board_no_thread.new_events_queue.get() == EVENT1
+
+
+def test_build_join(dummy_event_board: EventBoard):
+    dummy_event_board.build()
+    dummy_event_board.notify_event(KILL_EVENT.event_name)
+    dummy_event_board.join()
 
 
 def test_event_loop(dummy_event_board: EventBoard):
@@ -33,3 +48,5 @@ def test_event_loop(dummy_event_board: EventBoard):
         dummy_event_board.notify_event(event.event_name)
         executed_event = event_handler.wait()
         assert executed_event == event
+
+    dummy_event_board.join()
