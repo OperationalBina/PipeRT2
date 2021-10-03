9f973dc922527f334d467a4f0f1ead9aea2fa13e
Elon-Abulafia <elon945@gmail.com>
2021-10-03 16:06:44 +0300

    Add tests for different cases

--- tests/pipert/core/managers/networks/test_queue_network.py
+++ tests/pipert/core/managers/networks/test_queue_network.py
@@ -62,3 +62,49 @@ def test_link_multiple_sources(dummy_queue_network):
     for index, routine in enumerate(destination_routines):
         assert source_routine1.message_handler.output_queue._queues[index] == routine.message_handler.input_queue.get_queue(False)
         assert source_routine2.message_handler.output_queue._queues[index] == routine.message_handler.input_queue.get_queue(False)
+
+
+def test_link_not_process_safe_single_destination(dummy_queue_network):
+    source_routine = Mock()
+    source_routine.flow_name = "dummy"
+    destination_routines = (Mock(),)
+    destination_routines[0].input_queue = QueueWrapper()
+    destination_routines[0].flow_name = "dummy"
+    data_transmitter = Mock()
+
+    dummy_queue_network.link(source_routine, destination_routines, data_transmitter)
+
+    assert type(source_routine.message_handler.output_queue) == PublishQueue
+    assert source_routine.message_handler.output_queue._queues[0] == destination_routines[0].message_handler.input_queue.get_queue(False)
+
+
+def test_link_process_safe_single_destination(dummy_queue_network):
+    source_routine = Mock()
+    source_routine.flow_name = "dummy1"
+    destination_routines = (Mock(),)
+    destination_routines[0].input_queue = QueueWrapper()
+    destination_routines[0].flow_name = "dummy2"
+    data_transmitter = Mock()
+
+    dummy_queue_network.link(source_routine, destination_routines, data_transmitter)
+
+    assert type(source_routine.message_handler.output_queue) == PublishQueue
+    assert source_routine.message_handler.output_queue._queues[0] == destination_routines[0].message_handler.input_queue.get_queue(True)
+
+
+def test_link_process_safe_multiple_destinations(dummy_queue_network):
+    source_routine = Mock()
+    source_routine.flow_name = "dummy1"
+    destination_routines = (Mock(), Mock())
+    for destination_routine in destination_routines:
+        destination_routine.input_queue = QueueWrapper()
+    destination_routines[0].flow_name = "dummy1"
+    destination_routines[0].flow_name = "dummy2"
+    data_transmitter = Mock()
+
+    dummy_queue_network.link(source_routine, destination_routines, data_transmitter)
+
+    assert type(source_routine.message_handler.output_queue) == PublishQueue
+
+    for index, routine in enumerate(destination_routines):
+        assert source_routine.message_handler.output_queue._queues[index] == routine.message_handler.input_queue.get_queue(False)
