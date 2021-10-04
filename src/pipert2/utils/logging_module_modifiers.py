0b0afab5cb391d3a324ab7a593284c86b9e7033e
MayoG <dhtnthu9794048@gmail.com>
2021-10-04 16:22:19 +0300

    Create new log level to the logging module

---
+++ src/pipert2/utils/logging_module_modifiers.py
@@ -0,0 +1,27 @@
+import logging
+
+PIPE_INFRASTRUCTURE_LOG_LEVEL = 5
+
+
+def add_pipe_log_level():
+    """Add a new log level to the logging module named PIPE_INFRASTRUCTURE.
+    You can use this level using the plog method in the Logger.
+    The level of PIPE_INFRASTRUCTURE is below the DEBUG level (10).
+
+
+    Examples:
+        >>> import sys
+        >>> add_pipe_log_level()
+        >>> logger = logging.getLogger()
+        >>> logger.addHandler(logging.StreamHandler(sys.stdout))
+        >>> logger.plog("wow")
+
+    """
+
+    logging.addLevelName(PIPE_INFRASTRUCTURE_LOG_LEVEL, "PIPE_INFRASTRUCTURE")
+
+    def plog(self, message, *args, **kws):
+        if self.isEnabledFor(PIPE_INFRASTRUCTURE_LOG_LEVEL):
+            self._log(PIPE_INFRASTRUCTURE_LOG_LEVEL, message, args, **kws)
+
+    logging.Logger.plog = plog
