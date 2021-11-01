4a15946d8afbe631418d39d6274ba7aa0d27e2c1
MayoG <dhtnthu9794048@gmail.com>
2021-11-01 15:49:38 +0200

    Fix test

--- tests/unit/pipert/core/base/test_data_transmitter.py
+++ tests/unit/pipert/core/base/test_data_transmitter.py
@@ -1,5 +1,6 @@
 import sys
 import pytest
+from pipert2.core.base.data import Data
 from pipert2.core.base.transmitters import BasicTransmitter
 
 
@@ -24,7 +25,9 @@ def test_shared_memory_transmit_receive(dummy_shared_memory_transmitter):
     if sys.version_info.minor <= 7:  # TODO: Add multiprocessing test
         transmit_func = dummy_shared_memory_transmitter.transmit()
         receive_func = dummy_shared_memory_transmitter.receive()
-        data = {"data": b"AAA" * 5000, "short_data": b"AAA"}
+
+        data = Data()
+        data.additional_data = {"data": b"AAA" * 5000, "short_data": b"AAA"}
         return_data = transmit_func(data)
         assert data == receive_func(return_data)
 
