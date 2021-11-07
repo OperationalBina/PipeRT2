2a951c85b19d328bb7eb427810a1e167783c80f7
MayoG <dhtnthu9794048@gmail.com>
2021-11-07 10:22:18 +0200

    Add docs for the data class

--- pipert2/core/base/data.py
+++ pipert2/core/base/data.py
@@ -3,4 +3,19 @@ from pipert2 import dataclass, field
 
 @dataclass
 class Data:
+    """Base class for the data moving in the pipeline.
+    User can implement Data classes for his data types.
+
+    Example usage:
+    >>> import numpy as np
+    >>> @dataclass
+    ... class FrameData(Data):
+    ...     frame: np.ndarray
+    ...     source: str
+
+    >>> frame = np.zeros((4,5))
+    >>> data = FrameData(frame=frame, source="Doctest")
+
+    """
+
     additional_data: dict = field(default_factory=dict, init=False)
