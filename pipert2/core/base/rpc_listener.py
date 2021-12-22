87dd5c511f67a9d66cbf05ae49aac83edbadc48a
yarinshaked <yarin.shaked7@gmail.com>
2021-12-22 12:58:04 +0200

    add rpc listener and test

---
+++ pipert2/core/base/rpc_listener.py
@@ -0,0 +1,28 @@
+from pipert2.utils.consts import START_EVENT_NAME, STOP_EVENT_NAME, KILL_EVENT_NAME
+
+
+class RPCListener:
+    """A RPC listener which invokes pipeline cli commands
+
+    """
+
+    def __init__(self, pipe):
+        self.pipe = pipe
+
+    def start(self):
+        """Invokes the start event in the pipe
+
+        """
+        self.pipe.notify_event(event_name=START_EVENT_NAME)
+
+    def stop(self):
+        """Invokes the stop event in the pipe
+
+        """
+        self.pipe.notify_event(event_name=STOP_EVENT_NAME)
+
+    def kill(self):
+        """Invokes the kill event in the pipe
+
+        """
+        self.pipe.notify_event(event_name=KILL_EVENT_NAME)
---
+++ tests/unit/pipert/core/base/test_rpc_listener.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+import pytest
+from mock import Mock, patch
+from pipert2.core.base.rpc_listener import RPCListener
+from pipert2.utils.consts import START_EVENT_NAME, STOP_EVENT_NAME, KILL_EVENT_NAME
+
+
+@pytest.fixture
+def rpc_listener():
+    with patch('pipert2.core.base.pipe.Pipe') as mock_pipe:
+        return RPCListener(mock_pipe)
+
+
+def test_start(rpc_listener):
+    rpc_listener.start()
+    pipe_mock: Mock = rpc_listener.pipe
+    pipe_mock.notify_event.assert_called_with(event_name=START_EVENT_NAME)
+
+
+def test_stop(rpc_listener):
+    rpc_listener.stop()
+    pipe_mock: Mock = rpc_listener.pipe
+    pipe_mock.notify_event.assert_called_with(event_name=STOP_EVENT_NAME)
+
+
+def test_kill(rpc_listener):
+    rpc_listener.kill()
+    pipe_mock: Mock = rpc_listener.pipe
+    pipe_mock.notify_event.assert_called_with(event_name=KILL_EVENT_NAME)
