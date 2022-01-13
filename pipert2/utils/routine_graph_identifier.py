107c08dcfad4d0c754de31532350b04f5e9b4ae7
San Moshe <sanmoshe100@gmail.com>
2022-01-13 15:17:01 +0200

    Inferr routine type based on wires

---
+++ pipert2/utils/routine_graph_identifier.py
@@ -0,0 +1,23 @@
+from typing import Dict, List, Set, Tuple
+from pipert2.core.base.routine import Routine
+from pipert2.core.base.routines.runner_factory import FINAL_EXTENDED_RUN, GENERATOR_EXTENDED_RUN, INNER_EXTENDED_RUN
+from pipert2.core.base.wire import Wire
+
+
+def solve_graph(wires: List[Wire]) -> Dict[str, Set[Routine]]:
+    sources = set()
+    destinations = set()
+
+    for wire in wires:
+        sources.add(wire.source)
+
+        for routine in wire.destinations:
+            destinations.add(routine)
+
+    generator = sources - destinations
+    inner = sources.intersection(destinations)
+    final = destinations - sources
+
+    return {GENERATOR_EXTENDED_RUN: generator,
+            INNER_EXTENDED_RUN: inner,
+            FINAL_EXTENDED_RUN: final}
