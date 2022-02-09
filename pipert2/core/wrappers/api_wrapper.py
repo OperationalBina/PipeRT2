5f028fc8b05253b5b7a1aebd2fbdea07fcfad5c8
tomerdayan168 <tomerdayan168@gmail.com>
2022-02-09 11:45:45 +0200

    Add new route for getting the structure of the pipe

--- pipert2/core/wrappers/api_wrapper.py
+++ pipert2/core/wrappers/api_wrapper.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import flask
 from flask import Flask
 from flask import Response
@@ -18,11 +20,13 @@ class APIWrapper:
         """
 
         self.notify_callback = pipe.get_event_notify()
+        self.pipe = pipe
 
         self.app = Flask(__name__)
         self.app.add_url_rule("/start", "start", self.start, methods=['POST'])
         self.app.add_url_rule("/pause", "pause", self.pause, methods=['POST'])
         self.app.add_url_rule("/kill", "kill", self.kill, methods=['POST'])
+        self.app.add_url_rule("/pipe/structure", "pipe_structure", self.get_pipe_structure, methods=['GET'])
 
         self.app.add_url_rule("/routines/<routine_name>/events/<event_name>/execute/",
                               "routine_execute",
@@ -80,6 +84,24 @@ class APIWrapper:
             shutdown_hook()
 
         return Response(status=200)
+
+    def get_pipe_structure(self):
+        """Get the structure of the pipeline with the events its including.
+
+        Returns:
+            Dictionary of routines details, and custom events the pipe supported.
+            {
+                Routines: [
+                    {
+                        flow_name: xxx,
+                        routine_name: xxx,
+                        events: []
+                    }
+                ],
+                Events: []
+            }
+        """
+        return self.pipe.get_pipe_structure()
 
     def routine_execute(self, routine_name, event_name):
         """Execute custom event. Should get request with 'event_name' and with optional keys parameters.
--- pipert2/utils/socketio_logger/socket_handler.py
+++ pipert2/utils/socketio_logger/socket_handler.py
@@ -2,6 +2,8 @@ import logging
 import socketio
 from socketio.exceptions import SocketIOError
 
+from pipert2.utils.consts.emit_socket_names import LOG_NAME
+
 
 class SocketHandler(logging.Handler):
     def __init__(self, url, log_event_name):
@@ -13,7 +15,10 @@ class SocketHandler(logging.Handler):
 
     def emit(self, record: logging.LogRecord) -> None:
         if not self.sio.connected:
-            self.sio.connect(self.url)
+            try:
+                self.sio.connect(self.url)
+            except socketio.exceptions.ConnectionError:
+                return
         try:
             self.sio.emit(self.log_event_name, self.formatter.format(record))
         except SocketIOError:
