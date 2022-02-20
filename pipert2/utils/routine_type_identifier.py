34deda88c0fff79f2d342b9ad201b4f2e033df6a
San Moshe <sanmoshe100@gmail.com>
2022-02-20 14:17:10 +0200

    Docstring

--- pipert2/utils/routine_type_identifier.py
+++ pipert2/utils/routine_type_identifier.py
@@ -5,6 +5,25 @@ from pipert2.utils.consts import GENERATOR_ROUTINE, INNER_ROUTINE, FINAL_ROUTINE
 
 
 def infer_routines_types(wires: List[Wire]) -> Dict[str, Set[Routine]]:
+    """
+    Group routines by their inferred type.
+    Types:
+        GENERATOR_ROUTINE - Routine that only produces data and send via queue
+        INNER_ROUTINE - Routine that receives data from another source, produces new data and send it via queue
+        FINAL_ROUTINE - Routine that receives data from another source, performs manipulation and doesn't send
+        it to another routine.
+
+    Args:
+        wires: List of wires which describes connections in the Pipeline
+
+    Returns:
+        Dictionary of key-string and value-Set[Routine] where key is one of three:
+            GENERATOR_ROUTINE
+            INNER_ROUTINE
+            FINAL_ROUTINE
+        and value is a unique set of routines that matches that key by method conditions.
+
+    """
     sources = set()
     destinations = set()
 
@@ -21,4 +40,3 @@ def infer_routines_types(wires: List[Wire]) -> Dict[str, Set[Routine]]:
     return {GENERATOR_ROUTINE: generator,
             INNER_ROUTINE: inner,
             FINAL_ROUTINE: final}
-
