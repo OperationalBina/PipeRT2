615850ac3348a910de9ece5e1f0b31fbe5f8e9bc
yarinshaked <yarin.shaked7@gmail.com>
2022-03-06 16:20:36 +0200

    Prevent circular import

--- pipert2/core/__init__.py
+++ pipert2/core/__init__.py
@@ -1,5 +1,5 @@
-from .managers import QueueNetwork, Network, EventBoard
-from .handlers import QueueHandler, EventHandler, MessageHandler
 from .base import DataTransmitter, BasicTransmitter, SharedMemoryTransmitter, Payload, Wire, Message, Routine, Flow, \
     Pipe, Data, FrameData, FPSRoutine, get_runner_for_type, RoutinesSynchronizer, SynchroniserNode, validate_flow, \
     flow_validator
+from .handlers import QueueHandler, EventHandler, MessageHandler
+from .managers import QueueNetwork, Network, EventBoard
